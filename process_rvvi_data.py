f47551b07072150147c2c4ae975b3b4ddc04daf8
Jan Zeman <jan.zeman4@gmail.com>
2025-10-14 14:56:02 +0200

    Parameterize script

--- process_rvvi_data.py
+++ process_rvvi_data.py
@@ -3,15 +3,15 @@ import pandas as pd
 year = 24
 
 # Load the Excel file
-df = pd.read_excel('24_evaluations.xlxs', header=2)
+df = pd.read_excel('M1_VO_posudky_H20-H24.xlsx', header=2)
 
 # Filter rows where 'Období' column contains 'H20'
 filtered_df = df[ 
-    (df['Období'] == 'H24') & 
+    (df['Období'] == ('H' + str(year))) & 
     (df['Organizační jednotka'] == 'České vysoké učení technické v Praze/Fakulta stavební') 
-]
+].copy()
 
 filtered_df.sort_values(by=['Obor (Ford)', 'Název výsledku', 'Vypracoval'], inplace=True)
 
 # Save the filtered data to a new Excel file
-filtered_df.to_excel('24_evaluations_filtered.xlsx', index=False)
+filtered_df.to_excel(str(year) + '_evaluations_filtered.xlsx', index=False)
